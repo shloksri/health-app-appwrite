ad7731f604c2e0340be0cfe46dc5951eaabc4110
Shlok <shloksrivastava7@gmail.com>
2024-10-30 21:09:16 +0530

    fix: endpoint

--- functions/main.js
+++ functions/main.js
@@ -9,8 +9,8 @@ export default async ({ req, res, log, error }) => {
 
     const client = new Client()
     client
-        .setEndpoint()
-        .setProject()
+        .setEndpoint('https://cloud.appwrite.io/v1')
+        .setProject(PROJECT_ID)
 
     const db = new Databases(client)
     if (req.method == 'GET') {
