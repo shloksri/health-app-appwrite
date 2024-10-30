0b44a9be1c07b96ad316b0ae7f5b12c4037751f0
Shlok <shloksrivastava7@gmail.com>
2024-10-30 21:04:45 +0530

    testing db listDocuments

--- functions/main.js
+++ functions/main.js
@@ -1,3 +1,26 @@
+import { Client, Databases } from "node-appwrite";
+
+const PROJECT_ID = process.env.PROJECT_ID
+const DB_ID = process.env.DB_ID
+const COLLECTION_ID_PROFILES = process.env.DB_ID
+
+
 export default async ({ req, res, log, error }) => {
+
+    const client = new Client()
+    client
+        .setEndpoint()
+        .setProject()
+
+    if (req.method == 'GET') {
+        const response = await db.listDocuments(
+            DB_ID,
+            COLLECTION_ID_PROFILES
+        )
+        return res.json(response.documents)
+    }
+
+
+
     return res.send("Shlok, tesing if user delete worked")
 }
