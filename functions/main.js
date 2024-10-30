f1e942af6ae82e6af2c4848e0e840eb23c91c615
Shlok <shloksrivastava7@gmail.com>
2024-10-30 22:11:57 +0530

    adding direct IDs

--- functions/main.js
+++ functions/main.js
@@ -1,22 +1,17 @@
 import { Client, Databases } from "node-appwrite";
 
-const PROJECT_ID = '671f17fb0030e3b17ebc'
-const DB_ID = '672258ee000b485826b6'
-const COLLECTION_ID_PROFILES = '672258f600298c20687b'
-
-
 export default async ({ req, res, log, error }) => {
 
     const client = new Client()
     client
         .setEndpoint('https://cloud.appwrite.io/v1')
-        .setProject(PROJECT_ID)
+        .setProject('671f17fb0030e3b17ebc')
 
     const db = new Databases(client)
     if (req.method == 'GET') {
         const response = await db.listDocuments(
-            DB_ID,
-            COLLECTION_ID_PROFILES
+            '672258ee000b485826b6',
+            '672258f600298c20687b'
         )
         return res.json(response.documents)
     }
