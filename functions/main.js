c653bb767453c3470f7e6b345f357e278abe0bc7
Shlok <shloksrivastava7@gmail.com>
2024-10-31 02:50:08 +0530

    update main.js

--- functions/main.js
+++ functions/main.js
@@ -1,27 +1,8 @@
-import { Client, Databases } from "node-appwrite";
-
-
-const PROJECT_ID = process.env.VITE_PROJECT_ID
-const DATABASE_ID = process.env.VITE_DATABASE_ID
-const COLLECTION_ID_ALLUSERS = process.env.VITE_COLLECTION_ID_ALLUSERS
-
 export default async ({ req, res, log, error }) => {
 
-    log(`Shlok logs IDs: Project: ${PROJECT_ID}, Database: ${DATABASE_ID}, Collection: ${COLLECTION_ID_ALLUSERS}`)
-    error(`Shlok logs errors: Project: ${PROJECT_ID}, Database: ${DATABASE_ID}, Collection: ${COLLECTION_ID_ALLUSERS}`)
-    const client = new Client()
-    client
-        .setEndpoint('https://cloud.appwrite.io/v1')
-        .setProject(PROJECT_ID)
+    log("Request body - ", req.body)
+    log("Request method - ", req.method)
+    error("Bhai error")
 
-    const db = new Databases(client)
-    if (req.method == 'GET') {
-        const response = await db.listDocuments(
-            DATABASE_ID,
-            COLLECTION_ID_ALLUSERS
-        )
-        return res.json(response.documents)
-    }
-
-    return res.send("Shlok, tesing if user delete worked")
+    return res.text("Check the Appwrite Console to see logs and errors!");
 }
