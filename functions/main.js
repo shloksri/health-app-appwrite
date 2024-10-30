986b77c263d1c02b6f5372850b23fc6fd400c708
Shlok <shloksrivastava7@gmail.com>
2024-10-30 22:19:38 +0530

    same

--- functions/main.js
+++ functions/main.js
@@ -1,5 +1,20 @@
 import { Client, Databases } from "node-appwrite";
 
 export default async ({ req, res, log, error }) => {
-    return res.send("Shlok, tesing if normal works")
+
+    const client = new Client()
+    client
+        .setEndpoint('https://cloud.appwrite.io/v1')
+        .setProject('671f17fb0030e3b17ebc')
+
+    const db = new Databases(client)
+    if (req.method == 'GET') {
+        const response = await db.listDocuments(
+            '672258ee000b485826b6',
+            '672258f600298c20687b'
+        )
+        return res.json(response.documents)
+    }
+
+    return res.send("Shlok, tesing if user delete worked")
 }
