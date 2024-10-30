3ad008a02faadd061038df2b5cba3380e76ce31d
Shlok <shloksrivastava7@gmail.com>
2024-10-31 04:11:47 +0530

    req.headers

--- functions/main.js
+++ functions/main.js
@@ -2,7 +2,9 @@ export default async ({ req, res, log, error }) => {
 
     log("Request body - ", req.body)
     log("Request method - ", req.method)
-    error("Bhai error")
+    // error("Bhai error")
+    const event = req.headers['x-appwrite-event'];
+    log("Request headers: ", event)
 
     return res.text("Check the Appwrite Console to see logs and errors!");
 }
