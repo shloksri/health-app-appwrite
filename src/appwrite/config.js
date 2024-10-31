116a8dfbfd0ff24a118e1a2492a2cf6ec08a78fa
Shlok <shloksrivastava7@gmail.com>
2024-10-31 12:23:42 +0530

    removing key

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -3,7 +3,7 @@ import { Client, Account, Databases, Storage, OAuthProvider } from 'appwrite'
 const client = new Client()
 client
     .setProject(import.meta.env.VITE_PROJECT_ID)
-    .setKey(import.meta.env.VITE_API_KEY)
+// .setKey(import.meta.env.VITE_API_KEY)
 
 const account = new Account(client);
 const databases = new Databases(client);
